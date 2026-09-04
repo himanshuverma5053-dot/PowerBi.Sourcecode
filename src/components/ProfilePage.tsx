8f46cea82ec164de8a36227db93a9744020e877f
himanshuverma5053-dot <himanshu.verma5053@gmail.com>
2026-09-04 20:39:37 +0530

    Update API URL in ProfilePage component

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -23,7 +23,7 @@ import {
   Sliders
 } from 'lucide-react';
 
-export const YOUR_API_URL_HERE = 'https://o5skhjqub2.execute-api.us-east-1.amazonaws.com/Prod';
+export const YOUR_API_URL_HERE = 'https://wsl820vpr8.execute-api.us-east-1.amazonaws.com/DataAPI';
 export const AWS_PROFILE_INVOKE_URL = YOUR_API_URL_HERE;
 
 interface ProfilePageProps {
