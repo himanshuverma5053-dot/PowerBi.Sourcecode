cc5498ab72f402ff78f10ee4b495cf7752b18fe9
himanshuverma5053-dot <himanshu.verma5053@gmail.com>
2026-09-07 00:07:43 +0530

    Update API URL to remove 'userdata' endpoint

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -21,7 +21,7 @@ import {
   AlertCircle,
 } from 'lucide-react';
 
-export const YOUR_API_URL_HERE = 'https://rauqc7kcx2.execute-api.us-east-1.amazonaws.com/Prod/userdata';
+export const YOUR_API_URL_HERE = 'https://rauqc7kcx2.execute-api.us-east-1.amazonaws.com/Prod';
 export const AWS_PROFILE_INVOKE_URL = YOUR_API_URL_HERE;
 
 interface ProfilePageProps {
